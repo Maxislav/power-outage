f06d19d3f18ef6df9090b257500b2751a6ce1831
Maksym-Lipatov_aswatson <Maksym_Lipatov@epam.com>
2026-01-01 22:53:57 +0200

    b

--- src/server.cjs
+++ src/server.cjs
@@ -24,31 +24,24 @@ const ALLOWED_ORIGINS = [
   "https://another-site.net",
 ];
 
-const headers = {
-  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  'accept-language': 'en-US,en;q=0.9,ru-UA;q=0.8,ru;q=0.7',
-  'cache-control': 'max-age=0',
-  'priority': 'u=0, i',
-  'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
-  'sec-ch-ua-mobile': '?0',
-  'sec-ch-ua-platform': '"Linux"',
-  'sec-fetch-dest': 'document',
-  'sec-fetch-mode': 'navigate',
-  'sec-fetch-site': 'none',
-  'sec-fetch-user': '?1',
-  'upgrade-insecure-requests': '1',
-  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-  'cookie': 'visid_incap_2224657=C4qg3tynR46X3ZinBES6SsXXVmkAAAAAQUIPAAAAAACkinURo/gzrOpJBRB1llIW; incap_ses_689_2224657=U4yMY05tHhat3DqrS9KPCcXXVmkAAAAA2QNLvabOxdACNwjD06yLmg==; dtek-kem=1gccm48kedg9qd696ss06anmu3; _language=1f011804d107a9f0f6fa36417ed49140e5bc2106c740e65666f3a94e857201cca%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_language%22%3Bi%3A1%3Bs%3A2%3A%22uk%22%3B%7D; _csrf-dtek-kem=39e2be2a9d4597c71ba9a56342e622da0e0c2e7402689161494dfdd43baa7604a%3A2%3A%7Bi%3A0%3Bs%3A14%3A%22_csrf-dtek-kem%22%3Bi%3A1%3Bs%3A32%3A%22mJHXwWO23-MKA2yzHl6XZxyN_Fil2pQi%22%3B%7D; _ga=GA1.3.334945741.1767299050; _gid=GA1.3.763767310.1767299051; _hjSessionUser_5026684=eyJpZCI6ImYwNDI1MTZiLWIzOTMtNWIwOS04YjIzLTFiYTAyZjczNGYxYSIsImNyZWF0ZWQiOjE3NjcyOTkwNTA5NjIsImV4aXN0aW5nIjpmYWxzZX0=; _hjSession_5026684=eyJpZCI6IjE1OTBmYWMxLWU1NGQtNDcwYi04ODBiLWEyYTcxODZhZGYxMSIsImMiOjE3NjcyOTkwNTA5NjQsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjoxLCJzcCI6MH0=; incap_wrt_373=69dWaQAAAAC5je1eGgAI9QIQlfLVz4oCGJey28oGIAIox6/bygYwAVL7gYqHxQlMRQoTnNldEDs=; _ga_DLFSTRRPM2=GS2.1.s1767299050$o1$g1$t1767299121$j60$l0$h0'
-};
+// const headers = {
+//   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+//   'accept-language': 'en-US,en;q=0.9,ru-UA;q=0.8,ru;q=0.7',
+//   'cache-control': 'max-age=0',
+//   'priority': 'u=0, i',
+//   'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Google Chrome";v="134"',
+//   'sec-ch-ua-mobile': '?0',
+//   'sec-ch-ua-platform': '"Linux"',
+//   'sec-fetch-dest': 'document',
+//   'sec-fetch-mode': 'navigate',
+//   'sec-fetch-site': 'none',
+//   'sec-fetch-user': '?1',
+//   'upgrade-insecure-requests': '1',
+//   'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+//   'cookie': 'visid_incap_2224657=C4qg3tynR46X3ZinBES6SsXXVmkAAAAAQUIPAAAAAACkinURo/gzrOpJBRB1llIW; incap_ses_689_2224657=U4yMY05tHhat3DqrS9KPCcXXVmkAAAAA2QNLvabOxdACNwjD06yLmg==; dtek-kem=1gccm48kedg9qd696ss06anmu3; _language=1f011804d107a9f0f6fa36417ed49140e5bc2106c740e65666f3a94e857201cca%3A2%3A%7Bi%3A0%3Bs%3A9%3A%22_language%22%3Bi%3A1%3Bs%3A2%3A%22uk%22%3B%7D; _csrf-dtek-kem=39e2be2a9d4597c71ba9a56342e622da0e0c2e7402689161494dfdd43baa7604a%3A2%3A%7Bi%3A0%3Bs%3A14%3A%22_csrf-dtek-kem%22%3Bi%3A1%3Bs%3A32%3A%22mJHXwWO23-MKA2yzHl6XZxyN_Fil2pQi%22%3B%7D; _ga=GA1.3.334945741.1767299050; _gid=GA1.3.763767310.1767299051; _hjSessionUser_5026684=eyJpZCI6ImYwNDI1MTZiLWIzOTMtNWIwOS04YjIzLTFiYTAyZjczNGYxYSIsImNyZWF0ZWQiOjE3NjcyOTkwNTA5NjIsImV4aXN0aW5nIjpmYWxzZX0=; _hjSession_5026684=eyJpZCI6IjE1OTBmYWMxLWU1NGQtNDcwYi04ODBiLWEyYTcxODZhZGYxMSIsImMiOjE3NjcyOTkwNTA5NjQsInMiOjAsInIiOjAsInNiIjowLCJzciI6MCwic2UiOjAsImZzIjoxLCJzcCI6MH0=; incap_wrt_373=69dWaQAAAAC5je1eGgAI9QIQlfLVz4oCGJey28oGIAIox6/bygYwAVL7gYqHxQlMRQoTnNldEDs=; _ga_DLFSTRRPM2=GS2.1.s1767299050$o1$g1$t1767299121$j60$l0$h0'
+// };
 
 // Пример вызова
-fetch('https://www.dtek-kem.com.ua/ua/shutdowns', {
-  method: 'GET',
-  headers: headers
-})
-.then(res => res.text())
-.then(console.log);
-
 
 const server = https.createServer(options, async (req, res) => {
   const origin = req.headers.origin;
@@ -65,14 +58,20 @@ const server = https.createServer(options, async (req, res) => {
       console.log(`Получен запрос. Обращаюсь к ${EXTERNAL_URL}...`);
 
       // Делаем запрос к сайту ДТЭК
-      const response = await fetch(EXTERNAL_URL, {headers});
+     const response = await fetch(EXTERNAL_URL, {
+        headers: {
+          ...req.headers,
+          host: "www.dtek-kem.com.ua/ua/shutdowns",
+          referer: "https://www.dtek-kem.com.ua/ua/shutdowns"
+        },
+      });
       const data = await response.text();
-      console.log(data);
+      // console.log(data);
 
       // Отправляем результат клиенту
       //res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
       const dd = extractFactObject(data);
-      console.log("результат клиенту->>", dd);
+      //console.log("результат клиенту->>", dd);
       if (!dd) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         res.end(data);
@@ -96,7 +95,8 @@ const server = https.createServer(options, async (req, res) => {
 const server2 = http.createServer(async (req, res) => {
   const origin = req.headers.origin;
 
-  console.log("origin ->>", origin);
+  console.log("headers ->>", req.headers);
+  //console.log("origin ->>", origin);
   if (ALLOWED_ORIGINS.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
@@ -108,14 +108,20 @@ const server2 = http.createServer(async (req, res) => {
       console.log(`Получен запрос. Обращаюсь к ${EXTERNAL_URL}...`);
 
       // Делаем запрос к сайту ДТЭК
-      const response = await fetch(EXTERNAL_URL, {headers});
+      const response = await fetch(EXTERNAL_URL, {
+        headers: {
+          ...req.headers,
+          host: "www.dtek-kem.com.ua/ua/shutdowns",
+          referer: "https://www.dtek-kem.com.ua/ua/shutdowns"
+        },
+      });
       const data = await response.text();
-      console.log(data);
+      // console.log(data);
 
       // Отправляем результат клиенту
       //res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
       const dd = extractFactObject(data);
-      console.log("результат клиенту->>", dd);
+      //console.log("результат клиенту->>", dd);
       if (!dd) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         res.end(data);
