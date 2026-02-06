fc158594cda41fe44620757e9bdd5675a3eda48a
Maksym Lipatov <maksym_lipatov@epam.com>
2026-02-06 22:10:38 +0200

    clouds dynamic

--- src/helper.ts
+++ src/helper.ts
@@ -135,3 +135,22 @@ export function isObjectEmpty(obj: any) {
     obj.constructor === Object // Проверка, что это "простой" объект (не Date, не Map)
   );
 }
+
+export function getRandom (min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+export function getRandomCenter (min: number, max: number): number {
+    //return Math.floor(Math.random() * (max - min + 1)) + min;
+    const iterations = 5;
+    let sum = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        sum += Math.random();
+    }
+
+    // Получаем среднее от 0 до 1
+    const normalRandom = sum / iterations;
+
+    // Масштабируем под нужный диапазон
+    return Math.floor(normalRandom * (max - min + 1)) + min;
+}
